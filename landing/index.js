b84cd45b0340ec99ed8a87b98decf418878adec4
M <28828855+quantomistro@users.noreply.github.com>
2020-05-05 16:41:28 +0800

    Update index.js

--- landing/index.js
+++ landing/index.js
@@ -1,3 +1,4 @@
+/*
 alert("Remember to get the hex of the Splash background and apply that to primary colors for all pages!!!");
-alert("thanks :3");
 alert("also, choose a secondary color too please :)");
+*/
