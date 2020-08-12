26fd3c0acf3245f2f8865bc6ad64b364c493d13e
quantomistro <mahmoodsaifi38@gmail.com>
2020-08-12 15:42:36 +0800

    cleanup

--- join/index.js
+++
@@ -1,8 +0,0 @@
-const body = document.querySelector("body");
-
-function toggleTheme() {
-	body.classList.toggle("dark-mode");
-}
-
-const themeBtn = document.querySelector("#toggle-theme");
-themeBtn.addEventListener("click", toggleTheme);
